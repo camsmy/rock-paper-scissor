cfee4441930ef3aea2624972ecb593594f695d3b
camsmy <camille.monis@gmail.com>
2020-05-07 19:04:13 +0800

    v.4

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ const game = ()=>{
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const compHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll('.hands img');
+        
+        hands.forEach(hand =>{
+            hand.addEventListener("animationend",function(){
+                this.style.animation="";
+            })
+        })
+
         ///computer options (random generated)
         const compOptions=['rock','paper','scissors'];
 
@@ -28,10 +36,15 @@ const game = ()=>{
                 const compChoice = compOptions[compNum];
                 ///check who wins
 
-                compareHand(this.textContent,compChoice);
-                //update images
-                playerHand.src=`./assets/${this.textContent}.png`;
-                compHand.src = `./assets/${compChoice}.png`;
+                setTimeout(()=>{
+                    compareHand(this.textContent,compChoice);
+                    //update images
+                    playerHand.src=`./assets/${this.textContent}.png`;
+                    compHand.src = `./assets/${compChoice}.png`;
+                },2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                compHand.style.animation = "shakeComp 2s ease";
             });
         });
     };
@@ -48,8 +61,6 @@ const compareHand = (pChoice,cChoice)=>{
     const winner = document.querySelector('.winner');
     if(pChoice === cChoice){
         winner.textContent = "It is a tie!";
-        pScore++;
-        cScore++;
         updateScore();
         return;
     }
